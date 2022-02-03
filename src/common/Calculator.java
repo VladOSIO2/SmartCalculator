62c6447245d1411676dca65b1f93fabbb8577ec8
VladOSIO2 <vl4ds1mus@gmail.com>
2022-02-03 11:53:19 +0200

    Stage 3: count them all

--- src/common/Calculator.java
+++ src/common/Calculator.java
@@ -1,14 +1,12 @@
 package common;
 
 public class Calculator {
-    public static int execute(String equation) {
-        String[] numStr = equation.split("\\s+");
-        switch (numStr.length) {
-            case 1:
-                return Integer.parseInt(numStr[0]);
-            case 2:
-                return Integer.parseInt(numStr[0]) + Integer.parseInt(numStr[1]);
+    public static int sum(String numbers) {
+        String[] numStr = numbers.split("\\s+");
+        int sum = 0;
+        for (String num : numStr) {
+            sum += Integer.parseInt(num);
         }
-        return 0;
+        return sum;
     }
 }
--- src/common/Main.java
+++ src/common/Main.java
@@ -6,14 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
         while (scanner.hasNext()) {
             String equation = scanner.nextLine();
-            if (equation.equals("/exit")) {
-                System.out.println("Bye!");
-                break;
+            switch (equation.trim()) {
+                case "/exit" :
+                    System.out.println("Bye!");
+                    exit = true;
+                    break;
+                case "" :
+                    break;
+                case "/help" :
+                    System.out.println("The program calculates the sum of numbers");
+                    break;
+                default :
+                    System.out.println(Calculator.sum(equation));
             }
-            if (!equation.isBlank()) {
-                System.out.println(Calculator.execute(equation));
+            if (exit) {
+                break;
             }
         }
     }
